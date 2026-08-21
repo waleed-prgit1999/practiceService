0c85a1c39bf5ff12f1929f61eec378194b736eef
waleed <awaleed636@gmail.com>
2026-08-21 00:01:53 -0700

    initial commit

---
+++ src/test/java/com/example/practiceService/PracticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
